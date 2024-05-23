c6a37825e07f365f88079700718d5ce59189b113
x245394 <roscogenesis.orobia@telus.com>
2024-05-23 11:41:43 +0800

    Sort imports.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+// modules
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+// components
 import App from "./App.jsx";
+
+// static imports
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
